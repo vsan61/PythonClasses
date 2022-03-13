84d590ec4401d93ffde814d97282f224bbf18500
vsan61 <70624231+vsan61@users.noreply.github.com>
2022-03-13 12:36:50 +0530

    Add files via upload

---
+++ perimeter_circle.py
@@ -0,0 +1,5 @@
+#write a program to calculate perimeter of the circle with radius 20.50
+pi=3.14
+radius = float(input("Please enter radius"))
+perimeter = 2*pi*radius
+print(f"the primeter of the circle with {radius} is {perimeter}")
