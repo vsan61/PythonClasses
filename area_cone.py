027aabc5cce02c75e4656b4f178cb5691e8a2ebb
vsan61 <70624231+vsan61@users.noreply.github.com>
2022-03-13 12:57:38 +0530

    Add files via upload

---
+++ area_cone.py
@@ -0,0 +1,11 @@
+# write program to find area of the cone with radius 250
+pi = 3.14
+
+radius = int(input("Enter the radius of the cone"))
+
+height = float(input("Enter the height of the cone"))
+
+Area = 0.33*pi*radius*radius*height
+
+print(f"Area of the cone with {radius} and {height} is {Area}")
+
