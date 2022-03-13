5c9c36aaa9b6ef494f8fae1ce1668076c75008e6
vsan61 <70624231+vsan61@users.noreply.github.com>
2022-03-13 12:42:11 +0530

    Add files via upload

---
+++ area_circle.py
@@ -0,0 +1,4 @@
+#write program to calculate are of circle with radius 100.25
+pi = 3.14
+radius = float(input("enter the radius of the circle"))
+print(f"Area of the circle with {radius} is {pi*radius*radius}")
