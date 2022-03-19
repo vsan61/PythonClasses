20dd524f7057d065d9114c344711c6c933e0b1d1
vsan61 <70624231+vsan61@users.noreply.github.com>
2022-03-19 11:33:17 +0530

    Add files via upload

---
+++ string_conversions.py
@@ -0,0 +1,73 @@
+a = "Information_Technology"
+
+print(a[-2:0:-1])	golonhcet_noitamrofni
+print(a[5:-5:5])	mnh
+print(a[-5:5:-5])	ott
+print(a[5:-5:-5])	empty
+print(a[3:9:-1])	empty
+print(a[9:-10:-2])	empty
+print(a[2:-4:-4])	empty
+print(a[-4:2:-2])	lnctniar
+print(a[0:-1:-3])	empty
+print(a[-3:3:6])	empty
+print(a[::-3])	ylhtoaoi
+print(a[::-2])	yoohe_otmon
+print(a[2:-2:-2])	empty
+print(a[2:-2:2])	fraintcnl
+print(a[5:-5:5])	mnh
+print(a[3:-3:3])	oaothl
+print(a[::2])	ifraintcnlg
+print(a[::-5])	yn_an
+      
+
+      a ="python_is_using_interpreter"
+
+print(a[-2:0:-1])	eterpretni_gnisu_si_nohty
+print(a[5:-5:5])	nu_r
+print(a[-5:5:-5]_)	rnii
+print(a[5:-5:-5])	empty
+print(a[3:9:-1])	empty
+print(a[9:-10:-2])	empty
+print(a[2:-4:-4])	empty
+print(a[-4:2:-2])	epen_ns_inh
+print(a[0:-1:-3])	empty
+print(a[-3:3:6])	empty
+print(a[::-3])	rerngssnt
+print(a[::-2])	rtrrtigius_otp
+print(a[2:-2:-2])	empty
+print(a[2:-2:2])	to_suigitrrt
+print(a[5:-5:5])	nu_r
+print(a[3:-3:3])	h__i_tp
+print(a[::2])	pto_suigitrrtr
+print(a[::-5])	rpis_y
+      
+
+      a = "computer science"
+
+      print(a[-10:10:3])	es
+print(a[-5:-1:1])	ienc
+print(a[-8:-2:10])	 
+print(a[5::3])	t ic
+print(a[:5:2])	cmu
+print(a[10:14:2])	ce
+print(a[-10:-3:3])	ese
+print(a[5:12:5])	tc
+print(a[3:-3:3])	pese
+print(a[-5:-3:20])	i
+
+
+      a = "Google_chrome_browser"
+
+print(a[-10:10:3])	empty
+print(a[-5:-1:1])	owse
+print(a[-8:-2:10])	_
+print(a[5::3])	ehmbwr
+print(a[:5:2])	gol
+print(a[10:14:2])	oe
+print(a[-10:-3:3])	mbw
+print(a[5:12:5])	eo
+print(a[3:-3:3])	g_rer
+print(a[-5:-3:20])	o
+      
+
+      
